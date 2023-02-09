4bc3aacf31ed00dee84e49461501eb9f43cb7d0c
Fiesty-Cushion <neupane.braz07@gmail.com>
2023-02-09 20:47:26 +0545

    updated pdf contents

--- files.py
+++ files.py
@@ -1,25 +1,28 @@
 import requests
 import pdf2image
-from datetime import datetime 
 import os
 
-year = datetime.now().strftime('%Y')
-month = datetime.now().strftime('%m')
 
-def createJPG(filename):
-    month = "01"
-    url = f"http://pcampus.edu.np/wp-content/uploads/{year}/{month}/{filename}"
+def create_files(url, filename):
     response = requests.get(url)
     with open(f"docs/{filename}", 'wb') as pdf:
         pdf.write(response.content)
-
+    pdf.close()
     images = pdf2image.convert_from_path(f"docs/{filename}")
     for i in range(len(images)):   
          images[i].save('images/'+'page'+ str(i) +'.jpg', 'JPEG')
 
-def deleteImages():
-    directory = os.fsencode("images")
-    for file in os.listdir(directory):
+
+
+def del_files():
+    imgDirectory = os.fsencode("images")
+    for file in os.listdir(imgDirectory):
         filename = os.fsdecode(file)
         path = f"images/{filename}"
+        os.remove(path)
+
+    docsDirectory = os.fsencode("docs")
+    for file in os.listdir(docsDirectory):
+        filename = os.fsdecode(file)
+        path = f"docs/{filename}"
         os.remove(path)
